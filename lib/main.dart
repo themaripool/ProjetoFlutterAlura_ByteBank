1a738a10f6eb6fc389b3913edf6364fa2f445609
MariAndrade <marielama@outlook.com.br>
2021-02-16 14:52:15 -0300

    [ADD] Textfields and controllers

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,11 @@ class ByteBankApp extends StatelessWidget {
 }
 
 class FormularioTransferencia extends StatelessWidget {
+
+  final TextEditingController _controlladorCampoNumeroConta = TextEditingController();
+  final TextEditingController _controlladorCampoValor = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,6 +34,7 @@ class FormularioTransferencia extends StatelessWidget {
           child: Column(
             children: [
               TextField(
+                controller: _controlladorCampoNumeroConta,
                 style: TextStyle(fontSize: 24.0),
                 decoration: InputDecoration(
                   labelText: 'Número da conta',
@@ -38,6 +44,7 @@ class FormularioTransferencia extends StatelessWidget {
               ),
 
               TextField(
+                controller: _controlladorCampoValor,
                 style: TextStyle(fontSize: 24.0),
                 decoration: InputDecoration(
                   icon: Icon(Icons.monetization_on),
@@ -53,10 +60,20 @@ class FormularioTransferencia extends StatelessWidget {
                   child: Text("Confirmar"),
                   color: Colors.blue,
                   textColor: Colors.white,
-                  onPressed: () {},
+                  onPressed: () {
+                    debugPrint('clicou no confirmar');
+                    final int numeroConta = int.tryParse(_controlladorCampoNumeroConta.text);
+                    final double valor = double.tryParse(_controlladorCampoValor.text);
+
+                    if (numeroConta != null && valor != null){
+                      final transferenciaCriada = Transferencia(valor, numeroConta);
+
+                      debugPrint(transferenciaCriada.toString());
+                    }
+                  },
                 ),
               )
-              
+
             ],
           ),
         ));
@@ -103,4 +120,9 @@ class Transferencia {
   final double valor;
   final int numeroConta;
   Transferencia(this.valor, this.numeroConta);
+
+  @override 
+  String toString(){
+    return 'Transferencia {valor: $valor, numeroConta: $numeroConta}';
+  }
 }
