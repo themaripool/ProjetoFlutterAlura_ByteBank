317e51962fe1e71a7c83160d45bb6123486450e9
MariAndrade <marielama@outlook.com.br>
2021-02-16 13:07:03 -0300

    [ADD] Create Transference Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,101 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("Transferências"),),
-      body: ListaTransferencias(),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),),
-    ),
-  ));
+  runApp(ByteBankApp());
 }
-
 //StatefulWidget -> Consegue mudar o conteudo do widget de forma dinamica
 //StatelessWidget -> Nao consegue mudar o conteudo dinamicamente
 
-class ListaTransferencias extends StatelessWidget{
+class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: [
-            ItemTransferencia(Transferencia(200.0, 1000)),
-            ItemTransferencia(Transferencia(300.0, 1000)),
-            ItemTransferencia(Transferencia(400.0, 1000))
-        ],
-      );
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
   }
 }
 
-class ItemTransferencia extends StatelessWidget{
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Criando Transferência"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
 
+              TextField(
+                style: TextStyle(fontSize: 24.0),
+                decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Valor',
+                  hintText: '0000',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: RaisedButton(
+                  child: Text("Confirmar"),
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () {},
+                ),
+              )
+              
+            ],
+          ),
+        ));
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transferências"),
+      ),
+      body: Column(
+        children: [
+          ItemTransferencia(Transferencia(200.0, 1000)),
+          ItemTransferencia(Transferencia(300.0, 1000)),
+          ItemTransferencia(Transferencia(400.0, 1000))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
   final Transferencia _transferencia;
   ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-            child: ListTile(
-              leading: Icon(Icons.monetization_on),
-              title: Text(_transferencia.valor.toString()),
-              subtitle: Text(_transferencia.numeroConta.toString()),
-              )
-            );
+        child: ListTile(
+      leading: Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+    ));
   }
 }
 
